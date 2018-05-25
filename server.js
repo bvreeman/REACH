0b9c1acb43d3426aba09530924c3706bd6394269
bvreeman <bvreeman@gmail.com>
2018-05-25 18:04:45 -0500

    commit in order to pull

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
+require('dotenv').config()
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 require('nodemon');
 const exphbs = require('express-handlebars');
-const twilio = require('twilio');
+const client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+const express = ('express');
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.get('/testTwilio', function(req, res){
+    client.sendMessage({
+        to: '+15072591109',
+        from: '+16123245498',
+        body: 'This is a test'
+    }, function(err, data){
+        if(err)
+            console.log(err);
+        console.log(data);
+    })
+})
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
