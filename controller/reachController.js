acbbad3f65184e4ed05dfcaeee85dd0bf1597984
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-05-29 16:06:46 -0500

    got public assets folder linked for css rendering of handlebars pages, updated colors

--- controller/reachController.js
+++ controller/reachController.js
@@ -2,6 +2,11 @@ var express = require("express");
 
 var router = express.Router();
 
+// These lines of code for linking the CSS files for html rendering
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+router.use ('/', express.static(publicPath));
+
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
