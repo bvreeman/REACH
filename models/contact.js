ba0b5486637ff6d5e70028217e53657ad2bbcd21
bvreeman <bvreeman@gmail.com>
2018-05-29 18:41:45 -0500

    small indent change

--- models/contact.js
+++ models/contact.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-    var Contact = sequelize.define("contacts", {
-      text: DataTypes.STRING,
-      complete: DataTypes.BOOLEAN
-    });
-    return Contact;
-  };
+  const Contact = sequelize.define('contacts', {
+    text: DataTypes.STRING,
+    complete: DataTypes.BOOLEAN,
+  });
+  return Contact;
+};
