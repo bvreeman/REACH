2ebd28e9d301ec127ed13761db2f229e7f267816
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-05-29 19:40:17 -0500

    fixing body parser to be called and used before requiring api-routes

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const db = require('./models');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 require('dotenv').config();
 
 require('./routes/api-routes.js')(app);
 
 const mysql = require('mysql');
-const bodyParser = require('body-parser');
+
 
 require('nodemon');
 const exphbs = require('express-handlebars');
