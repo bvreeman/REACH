c1d3fcabdde1326f21cd5644aeb89ce0c683884e
Sandra Peterson <writesandy@gmail.com>
2018-05-30 09:04:03 -0500

    formatted phone to database

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -4,16 +4,18 @@ $('#submit').on("click", function (event){
     event.preventDefault();
 
     var newPhone = $('#phone').val().trim();
+    var formattedPhone = ('+1' +newPhone)
     var newMessage = $('#message').val().trim();
-    // var newDate = $('#datePicker').val().trim();
+    var newDate = $('#datePicker').val().trim();
     // var newTime = $('#timePicker').val().trim();
    
+    console.log(formattedPhone);
 
     $.ajax({
         method: "POST",
         url: "/api/getNumber",
         data: {
-            phone_number: newPhone,
+            phone_number: formattedPhone,
             outgoing_message: newMessage,
             // scheduled_date: newDate,
             // scheduled_time: newTime
