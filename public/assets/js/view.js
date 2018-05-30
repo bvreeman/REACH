122f5fbd1b3f9d6d7ea50059318cd306b61221c2
Sandra Peterson <writesandy@gmail.com>
2018-05-30 10:31:08 -0500

    date format into mysql db

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -7,9 +7,13 @@ $('#submit').on("click", function (event){
     var formattedPhone = ('+1' +newPhone)
     var newMessage = $('#message').val().trim();
     var newDate = $('#datePicker').val().trim();
+    var formattedDate = moment({newDate}).format("YYYY-MM-DD");
     // var newTime = $('#timePicker').val().trim();
-   
+    // var formattedTime = moment({newTime}).format("HH:MM:ss a");
+    
+    console.log(formattedDate);
     console.log(formattedPhone);
+    // console.log(formattedTime);
 
     $.ajax({
         method: "POST",
@@ -17,7 +21,7 @@ $('#submit').on("click", function (event){
         data: {
             phone_number: formattedPhone,
             outgoing_message: newMessage,
-            // scheduled_date: newDate,
+            scheduled_date: formattedDate,
             // scheduled_time: newTime
         }
         }).then(
