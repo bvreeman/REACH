30f7b54dcac2d843cb7cff93d39b9df2cd65e182
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-05-30 19:54:03 -0500

    formatted date and time automatic input for date and time input fields

--- public/assets/js/dateTimePickers.js
+++ public/assets/js/dateTimePickers.js
@@ -2,10 +2,20 @@
 
 
 $( document ).ready(function() {
-    $("#datePicker").empty();
-    $("#datePicker").val(moment());
-    $("#datePicker").datepicker();
+
+    // Setup side navbar
     $('.sidenav').sidenav();
+
+    // Get current date in date input field
+    $("#datePicker").empty();
+    $("#datePicker").val(moment().format("MMM DD, YYYY"));
+    // Setup date modal for date input field
+    $("#datePicker").datepicker();
+
+
+    $('#timePicker').empty();
+    $('#timePicker').val(moment().format("hh:mm A"));
+    // Setup time modal for time input field
     $('#timePicker').timepicker();
 
 } );
