b1a6f4f5ad0ff96945a2fa42d5c7afd53b331e5b
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-06-01 11:20:23 -0500

    made change to jquery event listener for delete button

--- public/assets/js/dateTimePickers.js
+++ public/assets/js/dateTimePickers.js
@@ -17,7 +17,17 @@ $(document).ready(function() {
     const timePickElems = document.querySelectorAll('#timePicker');
     // Initializing formatting and options for timePicker modals
     const timePickInstances = M.Timepicker.init(timePickElems, {
-        autoClose: true,
+        autoClose: true
+        // onSelect: function(hour, minute) {
+        //     const formattedSelectedTime = {
+        //         selectedHour: hour,
+        //         selectedMinute: minute,
+        //     };
+        //     console.log(`this is the Date: ${formattedSelectedDate.selectedYear}-${formattedSelectedDate.selectedMonth}-${formattedSelectedDate.selectedDay}`);
+        //     $('#datePicker').val(moment(`${formattedSelectedTime.selectedHour}:${formattedSelectedDate.selectedMonth}-${formattedSelectedDate.selectedDay}`, ['YYYY-MM-DD']).format('MMM DD, YYYY'));
+        //     console.log(`New Value:${$('#datePicker').val()}`);
+        //     datePickInstance.close();
+        // },
     });
 
     // We currently only have one datepicker input on our home page, therefore we are only concerned with the first instance
@@ -29,7 +39,9 @@ $(document).ready(function() {
     //     timePickInstance.open();
     //     console.log("focusing on the time field");
     // });
-
+    // $("#timePicker").change(function() {
+    //     $('#submit').focus();
+    // });
 
   // DATEPICKER
 
@@ -41,16 +53,16 @@ $(document).ready(function() {
         minDate: new Date(moment().format('YYYY,M,D')),
         // onSelect method for updating the DOM date input field with the selected date and autoClosing the modal when the date is selected
         onSelect: function(time) {
-        const selectedDate = new Date(time);
-        const formattedSelectedDate = {
-            selectedDay: selectedDate.getDate(),
-            selectedMonth: selectedDate.getMonth() + 1,
-            selectedYear: selectedDate.getFullYear(),
-        };
-        console.log(`this is the Date: ${formattedSelectedDate.selectedYear}-${formattedSelectedDate.selectedMonth}-${formattedSelectedDate.selectedDay}`);
-        $('#datePicker').val(moment(`${formattedSelectedDate.selectedYear}-${formattedSelectedDate.selectedMonth}-${formattedSelectedDate.selectedDay}`).format('MMM DD, YYYY'));
-        console.log(`New Value:${$('#datePicker').val()}`);
-        datePickInstance.close();
+            const selectedDate = new Date(time);
+            const formattedSelectedDate = {
+                selectedDay: selectedDate.getDate(),
+                selectedMonth: selectedDate.getMonth() + 1,
+                selectedYear: selectedDate.getFullYear(),
+            };
+            console.log(`this is the Date: ${formattedSelectedDate.selectedYear}-${formattedSelectedDate.selectedMonth}-${formattedSelectedDate.selectedDay}`);
+            $('#datePicker').val(moment(`${formattedSelectedDate.selectedYear}-${formattedSelectedDate.selectedMonth}-${formattedSelectedDate.selectedDay}`, ['YYYY-MM-DD']).format('MMM DD, YYYY'));
+            console.log(`New Value:${$('#datePicker').val()}`);
+            datePickInstance.close();
         },
     });
     
@@ -61,6 +73,10 @@ $(document).ready(function() {
     $("#datePicker").focus(function() {
         datePickInstance.open();
         console.log("focusing on the date field");
+    });
+    $('#datePicker').change( function() {
+        timePickInstance.open();
+        $('#submit').focus();
     });
 
 } );
