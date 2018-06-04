98fb1d801590df4b84f53fd3b14cdc3e57f9895c
mrassett <magsrassett@gmail.com>
2018-06-04 01:26:26 -0500

    image not centered

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,67 +1,63 @@
 
-  $('#submit').on('click', function (event) {
-    event.preventDefault();
+$('#submit').on('click', function (event) {
+  event.preventDefault();
 
-    const newPhone = $('#phone').val().trim();
-    const formattedPhone = (`+1${newPhone}`);
-    const newMessage = $('#message').val().trim();
+  let newPhone = $('#phone').val().trim();
+  // const formattedPhone = (`+1${newPhone}`);
+  const newMessage = $('#message').val().trim();
 
-    // For home.handlebars:
-    // const newDate = $('#datePicker').val().trim();
-    // const formattedDate = moment(newDate, ['MMM DD, YYYY']).format('YYYY-MM-DD');
-    // const newTime = $('#timePicker').val();
-    // const formattedTime = moment(newTime, ['h:mm A']).format('HH:mm');
+  // For home.handlebars:
+  // const newDate = $('#datePicker').val().trim();
+  // const formattedDate = moment(newDate, ['MMM DD, YYYY']).format('YYYY-MM-DD');
+  // const newTime = $('#timePicker').val();
+  // const formattedTime = moment(newTime, ['h:mm A']).format('HH:mm');
 
-    // For home2.handlebars:
-    const newDate = `${$('#selectedMonth').val()} ${$('#selectedDay').val()}, ${$('#selectedYear').val()}`;
-    const formattedDate = moment(newDate, ['MM DD, YYYY']).format('YYYY-MM-DD');
-    const newTime = `${$('#selectedHour').val()}:${$('#selectedMinute').val()} ${$('#selectedAMPM').val()}`;
-    const formattedTime = moment(newTime, ['hh:mm A']).format('HH:mm');
-
-    
-    const dateTime = `${formattedDate} ${formattedTime}`;
-    const formattedDateTime = moment(dateTime, ['YYYY-MM-DD HH:mm']).format('YYYY-MM-DD HH:mm');
-    // console.log(formattedDate);
-    // console.log(formattedPhone);
-    // console.log(formattedTime);
-    console.log('\n<---------------------->\n');
-    console.log(`CHECK OUT THIS!: ${dateTime}`);
-    console.log('\n<---------------------->\n');
-
-    $.ajax({
-      method: 'POST',
-      url: '/api/getNumber',
-      data: {
-        phone_number: formattedPhone,
-        outgoing_message: newMessage,
-        // scheduled_date: formattedDate,
-        // scheduled_time: formattedTime,
-        scheduled_send: formattedDateTime,
-      },
-    }).then(function(toTheServer) {
-      console.log(toTheServer);
-    });
+  // For home2.handlebars:
+  const newDate = `${$('#selectedMonth').val()} ${$('#selectedDay').val()}, ${$('#selectedYear').val()}`;
+  const formattedDate = moment(newDate, ['MM DD, YYYY']).format('YYYY-MM-DD');
+  const newTime = `${$('#selectedHour').val()}:${$('#selectedMinute').val()} ${$('#selectedAMPM').val()}`;
+  const formattedTime = moment(newTime, ['hh:mm A']).format('HH:mm');
 
 
+  const dateTime = `${formattedDate} ${formattedTime}`;
+  const formattedDateTime = moment(dateTime, ['YYYY-MM-DD HH:mm']).format('YYYY-MM-DD HH:mm');
+  // console.log(formattedDate);
+  // console.log(formattedPhone);
+  // console.log(formattedTime);
+  console.log('\n<---------------------->\n');
+  console.log(`CHECK OUT THIS!: ${dateTime}`);
+  console.log('\n<---------------------->\n');
+
+  $.ajax({
+    method: 'POST',
+    url: '/api/getNumber',
+    data: {
+      phone_number: newPhone,
+      outgoing_message: newMessage,
+      // scheduled_date: formattedDate,
+      // scheduled_time: formattedTime,
+      scheduled_send: formattedDateTime,
+    },
+  }).then(function(toTheServer) {
+    console.log(toTheServer);
   });
-
-$("body").on("click", ".delete", function(event){
-  const id= $(this).data("id");
-    console.log('it works!');
-  $.ajax("/outbox/" +id, {
-    type: "DELETE",
-  }).then(
-    function() {
-      console.log("deleted message" +id)
-      location.reload();
-    }
-  )
 });
 
-$("body").on("click", ".edit", function(event){
+$('body').on('click', '.delete', function(event) {
+  const id = $(this).data('id');
+  console.log('it works!');
+  $.ajax(`/outbox/${id}`, {
+    type: 'DELETE',
+  }).then(function() {
+    console.log(`deleted message${id}`);
+    location.reload();
+  });
+});
+
+$('body').on('click', '.edit', function(event) {
   event.preventDefault();
-  const id= $(this).data("id");
-  window.location.href = "/edit/"+id
+  const id = $(this).data('id');
+  window.location.href = `/edit/${id}`;
 });
 
 $('#submit').on('click', function (event) {
@@ -71,7 +67,7 @@ $('#submit').on('click', function (event) {
     method: 'POST',
     url: '/api/getNumber',
     data: {
-      phone_number: formattedPhone,
+      phone_number: newPhone,
       outgoing_message: newMessage,
       // scheduled_date: formattedDate,
       // scheduled_time: formattedTime,
@@ -80,5 +76,4 @@ $('#submit').on('click', function (event) {
   }).then(function(toTheServer) {
     console.log(toTheServer);
   });
-
 });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('../models');
+
 const contacts = db.contacts;
 const moment = require('moment');
 
@@ -18,22 +19,24 @@ router.use('/', express.static(publicPath));
 router.get('/', function(req, res) {
   res.render('home2');
 });
+router.get('/about', function(req, res) {
+  res.render('about');
+});
 
-router.get("/outbox", function(req, res) {
+router.get('/outbox', function(req, res) {
   contacts.findAll({}).then(function(dbContacts) {
-    return res.render("outbox",{dbContacts: dbContacts});;
-
+    return res.render('outbox', { dbContacts: dbContacts });
   });
-});  
+});
 
-router.get("/edit/:id", function(req, res) {
-  contacts.findAll({where: {id:req.params.id}}).then(function(dbContacts) {
+router.get('/edit/:id', function(req, res) {
+  contacts.findAll({ where: { id: req.params.id } }).then(function(dbContacts) {
     console.log(dbContacts, dbContacts[0].dataValues.id);
-    let dateAndTime = dbContacts[0].dataValues.scheduled_send.split(' ');
-    let date = dateAndTime[0].split('-');
-    let formattedObject = {
+    const dateAndTime = dbContacts[0].dataValues.scheduled_send.split(' ');
+    const date = dateAndTime[0].split('-');
+    const formattedObject = {
       contact_name: dbContacts[0].dataValues.contact_name,
-      phone_number: dbContacts[0].dataValues.phone_number.replace("+1", ""),
+      phone_number: dbContacts[0].dataValues.phone_number.replace('+1', ''),
       email_address: dbContacts[0].dataValues.email_address,
       outgoing_message: dbContacts[0].dataValues.outgoing_message,
       year: date[0],
@@ -41,12 +44,11 @@ router.get("/edit/:id", function(req, res) {
       day: date[2],
       hour: moment(dateAndTime[1], ['HH:mm']).format('hh'),
       minute: moment(dateAndTime[1], ['HH:mm']).format('mm'),
-      ampm: moment(dateAndTime[1], ['HH:mm']).format('A')
-    }
-    return res.render("edit",formattedObject);;
-
+      ampm: moment(dateAndTime[1], ['HH:mm']).format('A'),
+    };
+    return res.render('edit', formattedObject);
   });
-});  
+});
 
 // Export routes for server.js to use.
 module.exports = router;
