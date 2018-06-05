34082fafb903365882730244926cd4bf566f604d
bvreeman <bvreeman@gmail.com>
2018-06-05 17:06:43 -0500

    no change committing to pull

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -47,9 +47,9 @@ $('body').on('click', '.edit', function(event) {
   window.location.href = `/edit/${id}`;
 });
 
-$("#update").on('click', function (event) {
+$('#update').on('click', function (event) {
   event.preventDefault();
-    
+
   const newPhone = $('#phone').val().trim();
   const formattedPhone = (`+1${newPhone}`);
   const newMessage = $('#message').val().trim();
@@ -68,7 +68,7 @@ $("#update").on('click', function (event) {
 
   $.ajax({
     method: 'PUT',
-    url: '/edit/' +messageId,
+    url: `/edit/${messageId}`,
     data: {
       phone_number: formattedPhone,
       outgoing_message: newMessage,
